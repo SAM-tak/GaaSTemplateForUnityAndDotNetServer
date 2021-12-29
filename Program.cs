773672c1dcde5b2864df0bfac719397a3eab83ee
SAM-tak <osamu.takasugi@gmail.com>
2021-12-29 22:36:03 +0900

    fix merge miss

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MessagePack;
 using MessagePack.AspNetCoreMvcFormatter;
-using YourProjectName.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 

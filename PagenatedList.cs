1f9a2c5a66c29159debe9a602934df1fbc2b2d8c
SAM-tak <osamu.takasugi@gmail.com>
2022-01-09 19:12:06 +0900

    Add PlayerAccout table View Manuary

--- PagenatedList.cs
+++ PagenatedList.cs
@@ -6,30 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace YourGameServer;
 
-public interface IPaginatedList
-{
-    int PageIndex { get; }
-    int TotalPages { get; }
-    bool HasPreviousPage { get; }
-    bool HasNextPage { get; }
-}
-
-public class PagingNavigationModel
-{
-    public PagingNavigationModel(IPaginatedList list)
-    {
-        List = list;
-    }
-    public PagingNavigationModel(string aspPage, IPaginatedList list)
-    {
-        AspPage = aspPage;
-        List = list;
-    }
-    public string AspPage { get; init; } = "./Index";
-    public IPaginatedList List { get; private set; }
-}
-
-public class PaginatedList<T> : List<T>, IPaginatedList
+public class PaginatedList<T> : List<T>
 {
     public int PageIndex { get; private set; }
     public int TotalPages { get; private set; }
@@ -41,10 +18,6 @@ public class PaginatedList<T> : List<T>, IPaginatedList
 
         AddRange(items);
     }
-
-    public bool HasPreviousPage => PageIndex > 1;
-
-    public bool HasNextPage => PageIndex < TotalPages;
 
     public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
     {

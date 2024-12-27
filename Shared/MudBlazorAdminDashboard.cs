abe60619456be739780cbc96b17252123943b621
SAM-tak <osamu.takasugi@gmail.com>
2024-12-27 17:59:26 +0900

    update to latest MudBlazor

--- Shared/MudBlazorAdminDashboard.cs
+++ Shared/MudBlazorAdminDashboard.cs
@@ -6,15 +6,15 @@ namespace YourGameServer.Shared
     {
         public MudBlazorAdminDashboard()
         {
-            Palette = new() {
-                Primary = Colors.Blue.Darken1,
-                Secondary = Colors.DeepPurple.Accent2,
-                Background = Colors.Grey.Lighten5,
-                AppbarBackground = Colors.Blue.Darken1,
-                DrawerBackground = "#FFF",
-                DrawerText = "rgba(0,0,0, 0.7)",
-                Success = "#06d79c"
-            };
+            // PaletteLight = new() {
+            //     Primary = Colors.Blue.Darken1,
+            //     Secondary = Colors.DeepPurple.Accent2,
+            //     Background = Colors.Gray.Lighten5,
+            //     AppbarBackground = Colors.Blue.Darken1,
+            //     DrawerBackground = "#FFF",
+            //     DrawerText = "rgba(0,0,0, 0.7)",
+            //     Success = "#06d79c"
+            // };
 
             LayoutProperties = new LayoutProperties() {
                 DefaultBorderRadius = "6px",

583a4307f6c9cf4e10814b403a5c0c626664176d
SAM-tak <osamu.takasugi@gmail.com>
2025-01-31 18:31:59 +0900

    a

--- YourGameServer.Game/Services/PlayerAccountService.cs
+++ YourGameServer.Game/Services/PlayerAccountService.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol;
 using YourGameServer.Game.Interface;
 using YourGameServer.Shared.Data;
+using YourGameServer.Shared.Models;
 using PlayerAccountStatus = YourGameServer.Game.Interface.PlayerAccountStatus;
 
 namespace YourGameServer.Game.Services;
@@ -25,7 +26,7 @@ public class PlayerAccountService(GameDbContext context, IHttpContextAccessor ht
         ulong playerId = ulong.Parse(_httpContextAccessor.HttpContext.Request.Headers["playerid"]);
         var playerAccount = await _context.PlayerAccounts.FindAsync(playerId)
             ?? throw new ReturnStatusException(StatusCode.NotFound, "correspond account was not found.");
-        return FormalPlayerAccount.FromPlayerAccount(playerAccount);
+        return FormalPlayerAccountFromPlayerAccount(playerAccount);
     }
 
     [FromTypeFilter(typeof(RpcAuthAttribute))]
@@ -39,8 +40,42 @@ public class PlayerAccountService(GameDbContext context, IHttpContextAccessor ht
 
         if(ids != null && ids.Length > 0) {
             return await _context.PlayerAccounts.Include(i => i.Profile)
-                .Where(i => ids.Contains(i.Id) && (PlayerAccountStatus)i.Status < PlayerAccountStatus.Banned).Select(i => MaskedPlayerAccount.FromPlayerAccount(i)).ToListAsync();
+                .Where(i => ids.Contains(i.Id) && (PlayerAccountStatus)i.Status < PlayerAccountStatus.Banned).Select(i => MaskedPlayerAccountFromPlayerAccount(i)).ToListAsync();
         }
         return null;
     }
+
+    public static MaskedPlayerAccount MaskedPlayerAccountFromPlayerAccount(PlayerAccount playerAccount) => new()
+    {
+        Id = playerAccount.Id,
+        LastLogin = playerAccount.LastLogin,
+        Profile = playerAccount.Profile != null ? MaskedPlayerProfileFromPlayerProfile(playerAccount.Profile) : null
+    };
+
+    public static FormalPlayerAccount FormalPlayerAccountFromPlayerAccount(PlayerAccount playerAccount) => new()
+    {
+        Id = playerAccount.Id,
+        Code = playerAccount.Code,
+        Status = (PlayerAccountStatus)playerAccount.Status,
+        Since = playerAccount.Since,
+        LastLogin = playerAccount.LastLogin,
+        Profile = playerAccount.Profile != null ? FormalPlayerProfileFromPlayerProfile(playerAccount.Profile) : null
+    };
+
+    public static FormalPlayerProfile FormalPlayerProfileFromPlayerProfile(PlayerProfile playerProfile) => new()
+    {
+        Id = playerProfile.Id,
+        OwnerId = playerProfile.OwnerId,
+        LastUpdate = playerProfile.LastUpdate,
+        Name = playerProfile.Name,
+        Motto = playerProfile.Motto,
+        IconBlobId = playerProfile.IconBlobId
+    };
+
+    public static MaskedPlayerProfile MaskedPlayerProfileFromPlayerProfile(PlayerProfile playerProfile) => new()
+    {
+        Name = playerProfile.Name,
+        Motto = playerProfile.Motto,
+        IconBlobId = playerProfile.IconBlobId
+    };
 }

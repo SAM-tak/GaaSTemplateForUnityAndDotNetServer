00447b71f422b69c4474b86f3674dfaa15f65a77
SAM-tak <osamu.takasugi@gmail.com>
2025-02-13 20:20:29 +0900

    refactor IDCoder

--- YourGameServer.Shared/IDCoder.cs
+++ YourGameServer.Shared/IDCoder.cs
@@ -7,32 +7,19 @@ public static class IDCoder
     static SqidsEncoder<ulong>? _sqids = null;
     static SqidsEncoder<ulong>? _sqidsForLoginKey = null;
 
-    public static void Initialize(int seed = 234456, int seedForLoginKey = 592183)
-    {
-        _sqids = CreateEncoder(seed);
-        _sqidsForLoginKey = CreateEncoder(seedForLoginKey);
-    }
-
-    static SqidsEncoder<ulong>? CreateEncoder(int seed)
+    public static void Initialize(int seed = 234456)
     {
         var alphabets = "abcdefghijknpqrstuvxyz23456789".ToCharArray();
         new Random(seed).Shuffle(alphabets.AsSpan());
-        return new(new() { MinLength = 10, Alphabet = new string(alphabets) });
+        _sqids = new(new() { MinLength = 10, Alphabet = new string(alphabets) });
+        _sqidsForLoginKey = new();
     }
 
     public static string Encode(ulong id) => _sqids?.Encode(id) ?? string.Empty;
 
-    public static ulong Decode(string source)
-    {
-        var values = _sqids?.Decode(source);
-        return values != null ? values[0] : 0;
-    }
+    public static ulong Decode(string source) => _sqids?.Decode(source)?[0] ?? 0;
 
     public static string EncodeForLoginKey(ulong id) => _sqidsForLoginKey?.Encode(id) ?? string.Empty;
 
-    public static ulong DecodeFromLoginKey(string source)
-    {
-        var values = _sqidsForLoginKey?.Decode(source);
-        return values != null ? values[0] : 0;
-    }
+    public static ulong DecodeFromLoginKey(string source) => _sqidsForLoginKey?.Decode(source)?[0] ?? 0;
 }

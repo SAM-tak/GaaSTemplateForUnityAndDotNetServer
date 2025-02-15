b81ddd5c51f4af2927ded0dda79874ef05905f06
SAM-tak <osamu.takasugi@gmail.com>
2025-02-15 21:30:17 +0900

    a

--- YourGameServer.Explorer/Program.cs
+++ YourGameServer.Explorer/Program.cs
@@ -2,6 +2,7 @@ using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using MudBlazor.Services;
 using NLog;
 using NLog.Web;
@@ -39,23 +40,31 @@ try {
         builder.Services.AddDbContext<GameDbContext>(options => options.UseMySql(connectionString, new MariaDbServerVersion(new Version(10, 6, 5))));
     }
     // Add services to the container.
-    var authentication = builder.Services.AddAuthentication(options => {
-        if(builder.Environment.IsDevelopment()) {
-            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-        }
-    });
+    var authentication = builder.Services
+        .AddAuthentication(options => {
+            if(builder.Environment.IsDevelopment()) {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            }
+        })
+        .AddCookie() // this is neccesary
+        .AddCookie("OpenIdConnect");
+
     if(builder.Environment.IsDevelopment()) {
-        authentication.AddCookie(); // this is neccesary
-        authentication.AddCookie("OpenIdConnect");
         authentication.AddGoogle(options => {
             IConfigurationSection googleAuthNSection = builder.Configuration.GetSection("Authentication:Google");
-            options.ClientId = googleAuthNSection["ClientId"] ?? string.Empty;
-            options.ClientSecret = googleAuthNSection["ClientSecret"] ?? string.Empty;
+            options.ClientId = googleAuthNSection["ClientId"]
+                ?? throw new InvalidOperationException("Google OAuth 'ClientId' not found.");
+            options.ClientSecret = googleAuthNSection["ClientSecret"]
+                ?? throw new InvalidOperationException("Google OAuth 'ClientSecret' not found.");
             options.SaveTokens = true;
         });
     }
     //authentication.AddMicrosoftIdentityWebApp(builder.Configuration);
+
+    _ = builder.Services.AddAuthorization(options => {
+        options.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    });
 
     builder.Services.AddRazorPages();
     builder.Services.AddServerSideBlazor();
@@ -64,15 +73,14 @@ try {
     var app = builder.Build();
 
     app.UseStaticFiles();
+    app.UseCookiePolicy();
     app.UseRouting();
     app.UseAuthentication();
     app.UseAuthorization();
     // Configure the HTTP request pipeline.
-    if(app.Environment.IsDevelopment()) {
-        app.MapBlazorHub();
-        app.MapFallbackToPage("/_Host");
-    }
-    else {
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+    if(!app.Environment.IsDevelopment()) {
         app.UseExceptionHandler("/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
